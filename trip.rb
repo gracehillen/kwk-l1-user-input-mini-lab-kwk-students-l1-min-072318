28386c71892c86ce3f712a5467193dd955f14c3b
gracehillen <grahillen19@ahastars.org>
2018-07-23 20:32:38 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -1,5 +1,5 @@
 puts "What city would you like to go to?"
-city = gets.chomp.capitalize
+city = gets.capitalize.chomp
 puts "Where would you like to stay?"
 stay = gets.chomp
 puts "What sites do you want to visit?"
