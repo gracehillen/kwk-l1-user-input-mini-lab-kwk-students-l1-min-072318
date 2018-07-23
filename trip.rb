d17e60a440ef1f865c9a647b3bf62b704003a373
gracehillen <grahillen19@ahastars.org>
2018-07-23 20:36:12 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -4,9 +4,9 @@ puts "Where would you like to stay?"
 stay = gets.chomp.capitalize
 puts "What sites do you want to visit?"
 sites = gets.chomp
-puts "What food you want to eat?"
-food = gets.chomp
+puts "Where would you like to eat?"
+eat = gets.chomp
 puts "How many nights do you want to stay?"
 nights = gets.chomp
 
-puts "On my trip to #{city} I would like to stay at #{stay}. I want to visit #{sites} and eat #{food} food. I plan to stay for #{nights} nights"
+puts "On my trip to #{city} I would like to stay at #{stay}. I want to visit #{sites} and to eat at #{eat}. I plan to stay for #{nights} nights"
