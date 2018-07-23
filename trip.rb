fcdc1de1150413ee1f6fb271176e47424a689c32
gracehillen <grahillen19@ahastars.org>
2018-07-23 20:22:27 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -2,4 +2,7 @@ puts "Where would you like to stay?"
 stay = gets.chomp
 puts "What sites do you want to visit?"
 sites = gets.chomp
-puts ""
+puts "What food you want to eat?"
+food = gets.chomp
+puts "How many nights do you want to stay?"
+nights
